2a1c8c4c0b8824dd1b55fa3b3bbdcbb78e8f68ab
Unknown <sr.rizado@gmail.com>
2018-11-16 20:26:14 +0100

    rutas y componentes

--- LSE/src/app/app.module.ts
+++ LSE/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'; 
 
 
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { VideoComponent } from './video/video.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/galeria', pathMatch: 'full'},
+  {path: 'galeria', component:GaleriaComponent},
+  {path: 'video', component:VideoComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleriaComponent,
+    VideoComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ LSE/src/app/models/image.ts
@@ -0,0 +1,7 @@
+export class Image {
+    constructor(
+        public id:string,
+        public link:string,
+    ){}
+}
+
